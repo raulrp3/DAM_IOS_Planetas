9dcaf1d7fdf97ae9ac5c68c9f899a087a8a8cb4f
raulrp3 <raul.ramirez@escuelaestech.es>
2020-02-12 17:53:14 +0100

    Initial Commit

---
+++ DAM_IOS_Planetas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DAM_IOS_Planetas
+//
+//  Created by raul.ramirez on 12/02/2020.
+//  Copyright © 2020 Raul Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
